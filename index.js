142d1a1a9b39ed09b382c1fd1bc09dd6582b104a
krushnakumarpatle <71128604+krushnakumarpatle@users.noreply.github.com>
2022-02-02 16:18:33 +0530

    Add  Drum kit files

---
+++ index.js
@@ -0,0 +1,55 @@
+// document.querySelector("button").addEventListener("click",handleclick);
+
+// function handleclick()
+// {
+//     alert("i got click");
+// }
+// Anormous function
+// var numberOfDrum = document.querySelectorAll(".drum").length;
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var buttoninnerhtml = this.innerHTML;
+    makesound(buttoninnerhtml);
+  });
+}
+document.addEventListener("keypress" , function(event)
+{
+  makesound(event.key); 
+});
+
+function makesound(key)
+{
+  switch (key) {
+    case "w":
+      var tom1 = new Audio('tom-1.mp3');
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio('tom-2.mp3');
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio('tom-3.mp3');
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio('tom-4.mp3');
+      tom4.play();
+      break;
+    case "j":
+    var snare = new Audio('snare.mp3');
+    snare.play();
+    break;
+    case "k":
+      var kickbass = new Audio('kick-bass.mp3');
+      kickbass.play();
+      break;
+    case "l":
+      var crash = new Audio('crash.mp3');
+      crash .play();
+      break;
+    
+    default:
+      break;
+  }
+}
